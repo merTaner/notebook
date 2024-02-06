384200ae097310a326153ad0f82e0c9c3103870c
Mert Taner <merttaner44@gmail.com>
2024-02-06 11:03:42 +0300

    added solve of quesiton

--- neetcode/42. Trapping Rain Water.py
+++ neetcode/42. Trapping Rain Water.py
@@ -22,7 +22,11 @@ Constraints:
 
 Takeaway : 
 
+input : [0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]
 
+maxleft : 0 0 1 1 2 2 2 2 3 3 3 3
+maxright: 3 3 3 3 3 3 3 2 2 2 1 0
+min(l, r):0 0 1 1 2 2 2 2 2 2 1 0
 """
 
 class Solution:
@@ -49,7 +53,25 @@ class Solution:
     
 
     def trap(self, height: list[int]) -> int:
-        pass
+        
+        if not height: return 0
+
+        l, r = 0, len(height) - 1
+        left_max = height[l]
+        right_max = height[r]
+        res = 0
+
+        while l < r:
+            if left_max < right_max:
+                l += 1
+                left_max = max(left_max, height[l])
+                res += left_max - height[l]
+            else:
+                r -= 1
+                right_max = max(right_max, height[r])
+                res += right_max - height[r]
+
+        return res
 
 
 if __name__ == '__main__':
